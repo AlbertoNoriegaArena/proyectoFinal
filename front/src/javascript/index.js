b80c5c7767e90f626306a57f7dd386dc744eba72
curso06 <curso06@gmail.com>
2024-12-17 12:32:22 +0100

    primer ejemplo para hacer un get y crear tabla

--- front/src/javascript/index.js
+++ front/src/javascript/index.js
@@ -1,1 +1,17 @@
-$.get('http://localhost:1234/api/productos');
+
+
+// Solicitud GET con todos los productos
+$.get('http://localhost:1234/api/productos', function(data) {
+    
+    data.forEach(function(producto) {
+        let fila = `<tr>
+            <td>${producto.nombre}</td>
+            <td>${producto.descripcion}</td>
+            <td>${producto.cantidad}</td>
+            <td>${producto.precio}</td>
+        </tr>`;
+        $('#tablaProductos tbody').append(fila); 
+    });
+}).fail(function() {
+    alert('Error al cargar los productos');
+});
